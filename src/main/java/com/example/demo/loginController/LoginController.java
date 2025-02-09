87fb63761d21a99b25f83b49749278ecc87b3d10
wirefox21 <wirefox21@gmail.com>
2025-02-10 05:24:16 +0900

    Login ver2月14日・最初のコミット

---
+++ src/main/java/com/example/demo/loginController/LoginController.java
@@ -0,0 +1,29 @@
+package com.example.demo.loginController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.loginForm.LoginForm;
+
+@Controller
+@RequestMapping("/wirefox")
+public class LoginController {
+	
+	@GetMapping("/login")
+	public String init(Model model) {
+		model.addAttribute("form", new LoginForm());
+		return "login";
+	}
+	
+	@PostMapping("/postLogin")
+	public String create(@ModelAttribute LoginForm form) {
+		System.out.println(form.getUsername());
+		System.out.println(form.getPassword());
+		return "redirect:/wirefox/login";
+	}
+
+}
